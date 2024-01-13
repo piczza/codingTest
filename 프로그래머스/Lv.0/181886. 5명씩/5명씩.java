491186196bfb16dec17fb227b6bdac86c7d6d58e
Yeji <137299181+piczza@users.noreply.github.com>
2024-01-13 14:43:50 +0900

    [level Lv.0] Title: 5명씩, Time: 0.01 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181886. 5명씩/5명씩.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[(names.length - 1) / 5 + 1];
+        int idx = 0;
+        for (int i = 0; i < names.length; i+=5) {
+            answer[idx++] = names[i];
+        }
+        
+        return answer;
+    }
+}
