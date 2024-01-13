14838ea5494904f4a7251c1dde556621f078a548
Yeji <137299181+piczza@users.noreply.github.com>
2024-01-13 23:51:41 +0900

    [level Lv.0] Title: ad 제거하기, Time: 2.89 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strArr) {
+        return Arrays.stream(strArr).filter(x->!x.contains("ad"))
+                .toArray(String[]::new);
+    }
+}
