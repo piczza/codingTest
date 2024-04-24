9aa1124c73be48777326c6746cc4d987558479f5
Yeji <137299181+piczza@users.noreply.github.com>
2024-04-24 23:23:24 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int odd = 0;
+        int even = 0;
+        for (int num : num_list){
+            if(num % 2 == 0) even++;
+            else odd++;
+        }
+        
+        return new int[] {even, odd};
+    }
+}
