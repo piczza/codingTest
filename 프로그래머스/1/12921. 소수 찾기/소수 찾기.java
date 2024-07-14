b7e7465a87890c26be27721c418293529bd130b6
Yeji <137299181+piczza@users.noreply.github.com>
2024-07-14 10:47:25 +0900

    [level 1] Title: 소수 찾기, Time: 187.72 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -2,32 +2,18 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        ArrayList<Integer> primeList = new ArrayList<Integer>();
-        for (int i = 2 ; i <= n ; ){
-            int j = 0;
-            for ( ; j < primeList.size() && j <= (int)Math.sqrt(i); j++) {
-                if (i%primeList.get(j) == 0) {
-                    break;
-                }
-            }
-            if (j == primeList.size() || j == (int)Math.sqrt(i) + 1) {
-                primeList.add(i);
-            }
-            if (i<=3) {
-                i++;
-            }
-            else {
-                if (i%6 == 5){
-                    int suspect = (i / 6 + 1) * 6 + 1;
-                    i = suspect;
-                }
-                else {
-                    int suspect = (i / 6 + 1) * 6 - 1;
-                    i = suspect;
-                }
-            }
-        }
-        answer = primeList.size();
-        return answer;
+      for(int i = 2; i <= n; i++){
+          int j = 2;
+          int cnt = 0;
+          while(j <= (int)Math.sqrt(i)){
+              if(i % j == 0){
+                cnt += 1;
+                break;
+              } 
+              j += 1;
+          }
+          if(cnt == 0) answer += 1;
+      }
+      return answer;
     }
 }
