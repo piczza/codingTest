5435ce0e70d71cb61b8389e6a272213497c30c21
Yeji <137299181+piczza@users.noreply.github.com>
2024-08-18 09:40:18 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int a = (brown+4)/2;
+        int b = yellow+2*a-4;
+        int[] answer = {(int)(a+Math.sqrt(a*a-4*b))/2,(int)(a-Math.sqrt(a*a-4*b))/2};
+        return answer;
+    }
+}
