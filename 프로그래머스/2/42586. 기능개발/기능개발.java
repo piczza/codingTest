e4e28f1f83acc5e0f1a8d45d651087e1fd3958bb
Yeji <kimpizzzaa@gmail.com>
2024-09-06 13:31:08 +0900

    [level 2] Title: 기능개발, Time: 0.51 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            q.add((int) Math.ceil((double) (100 - progresses[i]) / speeds[i]));
+        }
+
+        int x = q.poll();
+        int count = 1;
+        while (!q.isEmpty()) {
+            if (x >= q.peek()) {
+                count++;
+                q.poll();
+            } else {
+                list.add(count);
+                count = 1;
+                x = q.poll();
+            }
+        }
+        list.add(count);
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+
+        return answer;
+    }
+}
